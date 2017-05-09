ec297810838d266b04a51c157914f887755a905f
vadiwalker <berinchik2012@gmail.com>
2017-05-10 01:50:47 +0300

    fast but have copypast

--- veb.cpp
+++ veb.cpp
@@ -174,8 +174,7 @@ class VEBTree<1> : AbstractVEBTree<1> {
 private:
 	unsigned long long min;
 	unsigned long long max;
-	unsigned int mask;
-	static const bool peril = true;
+	unsigned long long mask;
 
 public:
 
@@ -188,69 +187,317 @@ public:
 	}
 
 	void remove(unsigned long long x) {
-		mask &= ~(1 << x);
+		mask &= ~(1ULL << x);
 	}
 
 	void add(unsigned long long x) {
-		mask |= (1 << x);
+		mask |= (1ULL << x);
 	}
 
 	unsigned long long next(unsigned long long x) const {
-		if (peril) {
-			unsigned int remainder = mask & (~(1 << (x + 1) - 1));
-			if (remainder == 0) 
-				return NO;
-			return __builtin_ffs(remainder) - 1;		
-		} else {
-			// x++; 
-		}
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		if (remainder == 0) 
+			return NO;
+		return __builtin_ffsll(remainder) - 1;		
 	}
 
 	unsigned long long prev(unsigned long long x) const {
-		if (peril) {
-			unsigned int remainder = mask & ((1 << x) - 1);
-			if (remainder == 0)
-				return NO;
-			return 1 - (__builtin_clz(remainder) - 30);
-		} else {
-			// if (!x) 
-			// 	return NO;
-			// x--;
-			// while (x > 0) {
-			// 	if (mask & (1 << x)) 
-			// 		break;
-			// 	x--;
-			// }
-			// if (mask & (1 << x)) {
-			// 	return x;
-			// } else {
-			// 	return NO;
-			// }
-		}
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
 	}
 
 	unsigned long long getMin() const {
 		if (empty())
 			return NO;
-		if (peril) {
-			return __builtin_ffs(mask) - 1;	
-		} else {
-			// if (mask & 1) return 0;
-			// else return next(0);
-		}
+		return __builtin_ffsll(mask) - 1;	
 		
 	}
 
 	unsigned long long getMax() const {
 		if (empty()) 	 
 			return NO;
-		if (peril) {
-			return 1 /*2^S - 1*/ - (__builtin_clz(mask) - 30);
-		} else {
-			// return prev(2);
-		}
+		return 63 - __builtin_clzll(mask);
 	}
 };
+
+
+template<>
+class VEBTree<2> : AbstractVEBTree<2> {
+
+private:
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long mask;
+
+public:
+
+	bool empty() const {
+		return mask == 0;
+	}
+
+	VEBTree() : min(NO), max(NO), mask(0) {
+	
+	}
+
+	void remove(unsigned long long x) {
+		mask &= ~(1ULL << x);
+	}
+
+	void add(unsigned long long x) {
+		mask |= (1ULL << x);
+	}
+
+	unsigned long long next(unsigned long long x) const {
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		
+		if (remainder == 0) 
+			return NO;
+		unsigned long long ret = __builtin_ffsll(remainder) - 1;
+		return __builtin_ffsll(remainder) - 1;		
+	}
+
+	unsigned long long prev(unsigned long long x) const {
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
+	}
+
+	unsigned long long getMin() const {
+		if (empty())
+			return NO;
+		return __builtin_ffsll(mask) - 1;	
+		
+	}
+
+	unsigned long long getMax() const {
+		if (empty()) 	 
+			return NO;
+		return 63 - __builtin_clzll(mask);
+	}
+};
+
+template<>
+class VEBTree<3> : AbstractVEBTree<3> {
+
+private:
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long mask;
+
+public:
+
+	bool empty() const {
+		return mask == 0;
+	}
+
+	VEBTree() : min(NO), max(NO), mask(0) {
+	
+	}
+
+	void remove(unsigned long long x) {
+		mask &= ~(1ULL << x);
+	}
+
+	void add(unsigned long long x) {
+		mask |= (1ULL << x);
+	}
+
+	unsigned long long next(unsigned long long x) const {
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		if (remainder == 0) 
+			return NO;
+		return __builtin_ffsll(remainder) - 1;		
+	}
+
+	unsigned long long prev(unsigned long long x) const {
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
+	}
+
+	unsigned long long getMin() const {
+		if (empty())
+			return NO;
+		return __builtin_ffsll(mask) - 1;	
+		
+	}
+
+	unsigned long long getMax() const {
+		if (empty()) 	 
+			return NO;
+		return 63 - __builtin_clzll(mask);
+	}
+};
+
+
+
+template<>
+class VEBTree<4> : AbstractVEBTree<4> {
+
+private:
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long mask;
+
+public:
+
+	bool empty() const {
+		return mask == 0;
+	}
+
+	VEBTree() : min(NO), max(NO), mask(0) {
+	
+	}
+
+	void remove(unsigned long long x) {
+		mask &= ~(1ULL << x);
+	}
+
+	void add(unsigned long long x) {
+		mask |= (1ULL << x);
+	}
+
+	unsigned long long next(unsigned long long x) const {
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		if (remainder == 0) 
+			return NO;
+		return __builtin_ffsll(remainder) - 1;		
+	}
+
+	unsigned long long prev(unsigned long long x) const {
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
+	}
+
+	unsigned long long getMin() const {
+		if (empty())
+			return NO;
+		return __builtin_ffsll(mask) - 1;	
+		
+	}
+
+	unsigned long long getMax() const {
+		if (empty()) 	 
+			return NO;
+		return 63 - __builtin_clzll(mask);
+	}
+};
+
+template<>
+class VEBTree<5> : AbstractVEBTree<5> {
+
+private:
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long mask;
+
+public:
+
+	bool empty() const {
+		return mask == 0;
+	}
+
+	VEBTree() : min(NO), max(NO), mask(0) {
+	
+	}
+
+	void remove(unsigned long long x) {
+		mask &= ~(1ULL << x);
+	}
+
+	void add(unsigned long long x) {
+		mask |= (1ULL << x);
+	}
+
+	unsigned long long next(unsigned long long x) const {
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		if (remainder == 0) 
+			return NO;
+		return __builtin_ffsll(remainder) - 1;		
+	}
+
+	unsigned long long prev(unsigned long long x) const {
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
+	}
+
+	unsigned long long getMin() const {
+		if (empty())
+			return NO;
+		return __builtin_ffsll(mask) - 1;	
+		
+	}
+
+	unsigned long long getMax() const {
+		if (empty()) 	 
+			return NO;
+		return 63 - __builtin_clzll(mask);
+	}
+};
+
+
+template<>
+class VEBTree<6> : AbstractVEBTree<6> {
+
+private:
+	unsigned long long min;
+	unsigned long long max;
+	unsigned long long mask;
+
+public:
+
+	bool empty() const {
+		return mask == 0;
+	}
+
+	VEBTree() : min(NO), max(NO), mask(0) {
+	
+	}
+
+	void remove(unsigned long long x) {
+		mask &= ~(1ULL << x);
+	}
+
+	void add(unsigned long long x) {
+		mask |= (1ULL << x);
+	}
+
+	unsigned long long next(unsigned long long x) const {
+		unsigned long long remainder = mask & (~((1ULL << (x + 1)) - 1));
+		if (remainder == 0) 
+			return NO;
+		return __builtin_ffsll(remainder) - 1;		
+	}
+
+	unsigned long long prev(unsigned long long x) const {
+		unsigned long long remainder = mask & ((1ULL << x) - 1);
+		if (remainder == 0)
+			return NO;
+		return 63 - __builtin_clzll(remainder);
+	}
+
+	unsigned long long getMin() const {
+		if (empty())
+			return NO;
+		return __builtin_ffsll(mask) - 1;	
+		
+	}
+
+	unsigned long long getMax() const {
+		if (empty()) 	 
+			return NO;
+		return 63 - __builtin_clzll(mask);
+	}
+};
+
 
 void expect_true(bool val) {
 	if (!val) {
@@ -310,7 +557,7 @@ void enumerate(std::set <int> & s) {
 }
 
 void test() {
-	const int S = 36;
+	const int S = 48;
 	
 	VEBTree <S> t;
 	std::set <int> s;
@@ -319,7 +566,6 @@ void test() {
 	std::vector <int> elements;
 
 	for (int i = 0; i < n; i++) {
-		//int element = rand() % (1 << S);
 		int element = rand();
 		if (s.find(element) == s.end()) {
 			s.insert(element);
@@ -328,31 +574,24 @@ void test() {
 		}
 	}
 
-	// enumerate(t);
-	
-	// enumerate(s);
-
-	// rev_enumerate(s);
-
-	// rev_enumerate(t);
-
 	assert(size(t) == s.size());
 	assert(t.empty() == s.empty());
 
 	random_shuffle(elements.begin(), elements.end());
 
 	for (int i = 0; i < n; i++) {
-		// int element = rand() % (1 << S);
 		int element = rand();
 
 		auto it = s.upper_bound(element);
 		int x = t.next(element);
+
 
 		if (it == s.end()) {
 			expect_true(x == -1);
 		} else {
 			expect_true(x == *it);
 		}
+
 
 		it = s.lower_bound(element);
 		
@@ -380,58 +619,6 @@ void test() {
 				t.add(element);
 			}
 		}
-	}
-}
-
-void stress() {
-	const int S = 16;
-
-	const int n = 6;
-
-	const bool debug = false;
-
-	if (debug) {
-		while (true) {
-
-			VEBTree <S> t;
-			std::set <int> s;
-			std::unordered_map <int, bool> used;
-
-			for (int i = 0; i < n; i++) {
-				int element = rand() % (1 << S);
-
-				if (used.count(element)) {
-					continue;
-				}
-				used[element] = true;
-
-				t.add(element);
-				s.insert(element);
-			
-			}
-
-			if (s.size() != size(t)) {
-				std::cout << "YES\n";
-
-				enumerate(s);
-				enumerate(t);
-
-				break;
-			}
-		}	
-	} else {
-
-		VEBTree <S> t;
-		t.add(10);
-		t.add(15);
-		t.add(20);
-		t.remove(10);
-		t.add(50);
-		t.remove(20);
-		t.remove(50);
-		t.add(200);
-		t.add(100);
-		rev_enumerate(t);
 	}
 }
 
